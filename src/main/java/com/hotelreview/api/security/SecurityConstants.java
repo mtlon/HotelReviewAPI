8052a70d39eab3b4ac88bb0c2ec18a395e590393
m.tlon <marcin.tlon1@gmail.com>
2023-02-13 10:52:37 +0100

    Generate Token

---
+++ src/main/java/com/hotelreview/api/security/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.hotelreview.api.security;
+
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 70000;
+    public static final String JWT_SECRET = "secret";
+}
